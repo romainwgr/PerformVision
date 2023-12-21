14e3d8094c98e3265c99b7170ac1892153dc8851
Allan ABATUCI <allan.abatuci@edu.univ-paris13.fr>
2023-12-21 09:17:16 +0000

    action_check_pswd

--- Controllers/Controller_login.php
+++ Controllers/Controller_login.php
@@ -8,6 +8,17 @@ class Controller_login extends Controller
      */
     public function action_default()
     {
-        // TODO: Implement action_default() method.
+        $this->action_login_form();
     }
+    public function action_login_form(){
+        $this->render("login");
+    }
+
+    public function action_check_pswd(){
+        $m=Model::getModel();
+        if(isset($_POST['mail']) && isset($_POST['password'])){
+            return $m->checkMailPassword($_POST['mail'],$_POST['password'];
+        }
+    }
+
 }
