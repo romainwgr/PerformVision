181533caf5469198a2b7edd2722c1968a62d7c19
React4Ever <latifalex95@gmail.com>
2023-12-21 10:33:01 +0100

    add view_login and login_css

---
+++ Views/view_login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@700&family=Figtree:wght@700&display=swap" rel="stylesheet">
+ 
+     <title>Perform vision Login</title>
+     <meta charset="UTF-8">
+     <link rel="stylesheet" href="css/Login.css">
+</head>
+
+<div class="pic">
+    <div class="container">
+        
+        <h1>Connectez-vous !</h1>
+        <h3>Accéder au site de Perform Vision</h3>
+        <div class="inp">
+            <input id="input" type="text" id="username">
+            <label for="Username">Email</label>
+        </div>
+        <div style="display: flex;"><a id="oublie" href="#">Adresse email oubliée ?</a></div>
+        <div class="inp">
+            <input id="input" type="password" id="password">
+            <label for="Password">Mot de passe</label>
+        </div>
+        <div style="display: flex;"><a id="oublie" href="#">Mot de passe oublié ?</a></div>
+        <a class="button" href="#" >
+            <button type="button">Connexion</button>
+        </a>
+
+    </div>
+</div>
+</body>
+</html>
