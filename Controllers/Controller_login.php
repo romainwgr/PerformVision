98f8052a7a766df5080f83e36a63d5cca6d5dd97
React4Ever <latifalex95@gmail.com>
2023-12-21 09:06:08 +0100

    init Controller_login

---
+++ Controllers/Controller_login.php
@@ -0,0 +1,13 @@
+<?php
+
+class Controller_login extends Controller
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function action_default()
+    {
+        // TODO: Implement action_default() method.
+    }
+}
