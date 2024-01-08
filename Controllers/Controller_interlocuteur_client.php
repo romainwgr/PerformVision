7bbbf3061dbabc61ce99c8815d6fed3ac0c42ba6
React4Ever <latifalex95@gmail.com>
2024-01-08 09:06:36 +0100

    add Controller_interlocuteur_client

---
+++ Controllers/Controller_interlocuteur_client.php
@@ -0,0 +1,13 @@
+<?php
+
+class Controller_interlocuteur_client extends Controller
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function action_default()
+    {
+        // TODO: Implement action_default() method.
+    }
+}
