d98dc06b4dc8a8aedf110cfd2820c1c7f297649f
Allan ABATUCI <allan.abatuci@edu.univ-paris13.fr>
2024-01-08 09:22:27 +0000

    pagination

--- Controllers/Controller_interlocuteur_client.php
+++ Controllers/Controller_interlocuteur_client.php
@@ -10,4 +10,13 @@ class Controller_interlocuteur_client extends Controller
     {
         // TODO: Implement action_default() method.
     }
+        public function action_pagination(){
+        $db=Model::getModel();
+        if(isset($POST["page"])){
+            $this->render("interlocuteur_client",$data=[
+                "prestataires"=>$db->pagination($POST["page"])
+            ]);
+
+        }
+    }
 }
