aa82c212e96eb4ad85b09b1535774d8cb6958da7
India CABO <india.cabo@edu.univ-paris13.fr>
2024-01-12 08:10:52 +0000

    Update view_declaration_absence.php

--- Views/view_declaration_absence.php
+++ Views/view_declaration_absence.php
@@ -1,1 +1,26 @@
+<?php
+require 'view_begin.php';
+$menu = [['link' => '?controller=prestataire&action=missions', 'name' => 'Missions'],
+    ['link' => '?controller=prestataire&action=clients', 'name' => 'Clients']];
+require 'view_header.php';
+?>
+<div class="container">
+    <div class="form-abs">
+        <h1>Déclaration d'absence</h1>
+        <form action="">
+            <input type="text" placeholder="Prénom">
+            <input type="text" placeholder="Nom">
+            <input type="email" placeholder="Adresse email">
+            <input type="date" placeholder="Date">
+            <textarea name="motif" id="reason" placeholder="Motif de l'absence..."></textarea>
 
+        </form>
+        <div class="buttons">
+            <button id="left-button">Enregistrer</button>
+            <button id="right-button">Télécharger</button>
+        </div>
+    </div>
+</div>
+<?php
+require 'view_end.php';
+?>
