8fbe0c036b828d9e2b5b528be8564e427ed377f0
India CABO <india.cabo@edu.univ-paris13.fr>
2024-01-12 08:16:13 +0000

    Update view_end.php

--- Views/view_end.php
+++ Views/view_end.php
@@ -1,3 +1,7 @@
-</main>
+<footer>
+    <div class="nom-entreprise"><p>Perform Vision</p></div>
+    <div class="info-entreprise"><a href="#">Mentions Légales</a></div>
+    <div class="info-entreprise"><a href="#">Informations sur l'entreprise</a></div>
+</footer>
 </body>
 </html>
