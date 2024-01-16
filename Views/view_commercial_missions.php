2f2e7a0d1389a1415ca9f1f3ea46144dce967d19
Allan ABATUCI <allan.abatuci@edu.univ-paris13.fr>
2024-01-16 14:13:11 +0000

    Upload New File

---
+++ Views/view_commercial_missions.php
@@ -0,0 +1,15 @@
+<?php
+require 'view_begin.php';
+require 'view_header.php';
+?>
+
+<div class='main-contrainer'>
+    <div class="dashboard-container">
+        <h1>Missions</h1>
+        <?php require_once 'view_dashboard.php'; ?>
+    </div>
+</div>
+
+<?php
+require 'view_end.php';
+?>
