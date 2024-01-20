dd6c2b825702507689f99ac3f55895faf46084ca
React4Ever <latifalex95@gmail.com>
2024-01-20 11:09:41 +0100

    change link for admin

--- Views/view_consulte_bdl.php
+++ Views/view_consulte_bdl.php
@@ -3,40 +3,43 @@ require 'view_begin.php';
 require 'view_header.php';
 ?>
 <div class="bdl-container">
-    <table class="bdl-table">
-        <thead>
-            <tr class="bdl-head">
-                <th>Date</th>
-                <th><?= $bdl['type_bdl'] ?></th>
-                <th>Commentaire</th>
-            </tr>
-        </thead>
-        <tbody id="joursTableBody">
-            <?php foreach ($activites as $activite) : ?>
-                <tr>
-                    <td><?= $activite["date_bdl"] ?></td>
-                    <td><?php
-                        if (isset($activite['journee'])): if ($activite['journee']): echo 'Présent';
-                        else: echo 'Absent'; endif; endif;
-                        if (isset($activite['nb_heure'])): echo $activite['nb_heure']; endif;
-                        if (isset($activite['nb_demi_journee'])): echo $activite['nb_demi_journee']; endif;
-                        ?></td>
-                    <td><?= $activite["commentaire"] ?></td>
+    <div class="bdl__table">
+
+        <table class="bdl-table">
+            <thead>
+                <tr class="bdl-head">
+                    <th>Date</th>
+                    <th><?= $bdl['type_bdl'] ?></th>
+                    <th>Commentaire</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-    <?php if (str_contains($_GET['controller'], 'interlocuteur')): ?>
-    <div class="button-valide-container">
-        <button class="button-delete button-valide"
-            onclick="window.location='?controller=interlocuteur&action=valider_bdl&id=<?php echo $_GET['id'] ?>&valide=false'"
-            id="button-get-data">Contester
-        </button>
-        <button class="button-primary button-valide"
-            onclick="window.location='?controller=interlocuteur&action=valider_bdl&id=<?php echo $_GET['id'] ?>&valide=true'"
-            id="button-get-data">Valider
-        </button>
+            </thead>
+            <tbody id="joursTableBody">
+                <?php foreach ($activites as $activite) : ?>
+                    <tr>
+                        <td><?= $activite["date_bdl"] ?></td>
+                        <td><?php
+                            if (isset($activite['journee'])): if ($activite['journee']): echo 'Présent';
+                            else: echo 'Absent'; endif; endif;
+                            if (isset($activite['nb_heure'])): echo $activite['nb_heure']; endif;
+                            if (isset($activite['nb_demi_journee'])): echo $activite['nb_demi_journee']; endif;
+                            ?></td>
+                        <td><?= $activite["commentaire"] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
+    <?php if (str_contains($_GET['controller'], 'interlocuteur')): ?>
+        <div class="button-valide-container">
+            <button class="button-delete button-valide"
+                    onclick="window.location='?controller=interlocuteur&action=valider_bdl&id=<?php echo $_GET['id'] ?>&valide=false'"
+                    id="button-get-data">Contester
+            </button>
+            <button class="button-primary button-valide"
+                    onclick="window.location='?controller=interlocuteur&action=valider_bdl&id=<?php echo $_GET['id'] ?>&valide=true'"
+                    id="button-get-data">Valider
+            </button>
+        </div>
     <?php endif; ?>
 </div>
 <?php
